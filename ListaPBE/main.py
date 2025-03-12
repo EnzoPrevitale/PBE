c988d42ef8d98be524c4a4a1de74bcb5739cf891
Enzo Gabriel Previtale Silva <49513408876@>
2025-03-12 16:52:54 -0300

    Lista de exercícios 12/03/2025

---
+++ ListaPBE/main.py
@@ -0,0 +1,30 @@
+# Lista de exercícios PBE
+'''
+#01
+
+# Declarando a variável nome
+nome = "João"
+
+# Exibindo a variável na tela
+print(nome)
+'''
+#02
+'''
+# Declarando as variáveis a e b com os valores 5 e 10
+a = 5
+b = 10
+
+# Calculando e imprimindo a soma das variáveis
+print(f"a + b = {a + b}")
+
+# Calculando e imprimindo a subtração de a por b
+print(f"a - b = {a - b}")
+
+# Calculando e imprimindo o produto entre as variáveis
+print(f"a * b = {a * b}")
+
+# Calculando e imprimindo o quociente entre a e b
+print(f"a / b = {a / b}")
+'''
+#03
+
